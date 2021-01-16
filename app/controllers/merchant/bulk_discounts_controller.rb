8eeffe21c7615400759dadc21e52ddfb0e6ecfa4
Saundra <saundracatalina@gmail.com>
2021-01-16 12:01:33 -0700

    Add to find_merchant to new and create actions and create a discount with discount_params with flash message

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,15 +1,35 @@
 class Merchant::BulkDiscountsController < ApplicationController
+  before_action :find_discount, only: [:show]
+  before_action :find_merchant, only: [:index, :new, :create]
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
-    @discounts = Merchant.find(params[:merchant_id]).bulk_discounts
+    @discounts = @merchant.bulk_discounts
   end
 
   def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    BulkDiscount.create!(discount_params)
+    flash.notice = 'Discount has been created!'
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+  def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_discount
     @discount = BulkDiscount.find(params[:id])
   end
 
-  def new 
-
+  def discount_params
+    params.permit(:name, :quantity, :percent_discount, :merchant_id)
   end
 end
