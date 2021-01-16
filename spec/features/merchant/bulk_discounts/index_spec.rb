60a05cb777159e421e173dcfcb8f0809a853c4b5
Saundra <saundracatalina@gmail.com>
2021-01-15 20:54:16 -0700

    Update tests to include BulkDiscount being created with and showing a name on pages

--- spec/features/merchant/bulk_discounts/index_spec.rb
+++ spec/features/merchant/bulk_discounts/index_spec.rb
@@ -5,23 +5,26 @@ describe "bulk_discounts index page" do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     @merchant2 = Merchant.create!(name: 'Jewelry')
 
-    @merch1_disc1 = BulkDiscount.create!(quantity: 10, percent_discount: 10, merchant_id: @merchant1.id)
-    @merch1_disc2 = BulkDiscount.create!(quantity: 15, percent_discount: 20, merchant_id: @merchant1.id)
+    @merch1_disc1 = BulkDiscount.create!(name: "Hair Care Discount 1", quantity: 10, percent_discount: 10, merchant_id: @merchant1.id)
+    @merch1_disc2 = BulkDiscount.create!(name: "Hair Care Discount 2", quantity: 15, percent_discount: 20, merchant_id: @merchant1.id)
 
-    @merch2_disc1 = BulkDiscount.create!(quantity: 75, percent_discount: 75, merchant_id: @merchant2.id)
+    @merch2_disc1 = BulkDiscount.create!(name: "Jewelry Discount 1", quantity: 75, percent_discount: 75, merchant_id: @merchant2.id)
   end
 
   it "can see all my bulk discounts, their percent_discount, and quantity thresholds" do
     visit merchant_bulk_discounts_path(@merchant1)
 
     within("#discount-#{@merch1_disc1.id}") do
+      expect(page).to have_content(@merch1_disc1.name)
       expect(page).to have_content(@merch1_disc1.quantity)
       expect(page).to have_content(@merch1_disc1.percent_discount)
     end
     within("#discount-#{@merch1_disc2.id}") do
+      expect(page).to have_content(@merch1_disc2.name)
       expect(page).to have_content(@merch1_disc2.quantity)
       expect(page).to have_content(@merch1_disc2.percent_discount)
     end
+    expect(page).to_not have_content(@merch2_disc1.name)
     expect(page).to_not have_content(@merch2_disc1.quantity)
     expect(page).to_not have_content(@merch2_disc1.percent_discount)
   end
@@ -30,8 +33,13 @@ describe "bulk_discounts index page" do
 
     within("#discount-#{@merch1_disc1.id}") do
       click_link "Check out this discount!"
-
       expect(current_path).to eq(merchant_bulk_discount_path(@merchant1, @merch1_disc1))
+    end
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    within("#discount-#{@merch1_disc2.id}") do
+      click_link "Check out this discount!"
+      expect(current_path).to eq(merchant_bulk_discount_path(@merchant1, @merch1_disc2))
     end
   end
   it "has a link to create a new discount that takes you to a form" do
