f685f81820638bfd0bc7e01417928ee0b0282e0f
Saundra <saundracatalina@gmail.com>
2021-01-19 21:51:43 -0700

    Add test it shows the total_revenue_with_discount for a specific invoice

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-describe 'Admin Invoices Index Page' do
+describe 'Admin Invoices Show Page' do
   before :each do
     @m1 = Merchant.create!(name: 'Merchant 1')
+
+    @merch1_disc1 = BulkDiscount.create!(name: "Hair Care Discount 1", quantity: 9, percent_discount: 10, merchant_id: @m1.id)
+    @merch1_disc2 = BulkDiscount.create!(name: "Hair Care Discount 2", quantity: 14, percent_discount: 20, merchant_id: @m1.id)
 
     @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
     @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
@@ -12,10 +15,15 @@ describe 'Admin Invoices Index Page' do
 
     @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
     @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
-   
+    @item_3 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 30, merchant_id: @m1.id)
+    @item_4 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @m1.id)
+
     @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
     @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+    @ii_4 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_3.id, quantity: 20, unit_price: 30, status: 2)
+    @ii_5 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_4.id, quantity: 10, unit_price: 5, status: 2)
+
 
     visit admin_invoice_path(@i1)
   end
@@ -50,7 +58,6 @@ describe 'Admin Invoices Index Page' do
 
     expect(page).to_not have_content(@ii_3.quantity)
     expect(page).to_not have_content("$#{@ii_3.unit_price}")
-    expect(page).to_not have_content(@ii_3.status)
   end
 
   it 'should display the total revenue the invoice will generate' do
@@ -69,5 +76,7 @@ describe 'Admin Invoices Index Page' do
       expect(@i1.status).to eq('complete')
     end
   end
+  it "shows the total_revenue_with_discount for this invoice" do
+    expect(page).to have_content("Total Revenue With Discounts Applied: #{@i1.total_revenue_with_discount}")
+  end
 end
-
